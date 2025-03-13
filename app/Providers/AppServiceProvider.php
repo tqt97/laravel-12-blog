840cf87297324c6cbc913f49f4bb88e1b91558ad
tuantq <kutuanonline199@gmail.com>
2025-03-13 21:03:54 +0700

    strict model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /**
+         * Eloquent strict mode
+         *
+         * Prevents lazy loading
+         * Prevents silently discarding attributes.
+         * Prevents accessing missing attributes.
+         */
+        Model::shouldBeStrict();
     }
 }
